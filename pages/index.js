cc23ee285f74e73c303ced0610ab51334784363f
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-05-12 18:00:47 -0600

    accessed the API and display the name of the cryptocurrencies mapping through the filteredCoins

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import SearchBar from "../components/SearchBar/SearchBar";
 import Head from "next/head";
+import CoinList from "../components/CoinList";
 
-export default function Home() {
+export default function Home({ filteredCoins }) {
   return (
     <div>
       <Head>
@@ -11,6 +12,21 @@ export default function Home() {
       </Head>
 
       <SearchBar type="text" placeholder="Search" />
+      <CoinList filteredCoins={filteredCoins} />
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false"
+  );
+
+  const filteredCoins = await res.json();
+
+  return {
+    props: {
+      filteredCoins,
+    },
+  };
+};
